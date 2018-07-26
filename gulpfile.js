d0b20bf6f7427adab77b96e1273b568333df20ae
Artem Klochkov <Arthorror@gmail.com>
2018-07-26 23:02:22 +0300

    Apdate repo

--- gulpfile.js
+++ gulpfile.js
@@ -42,11 +42,6 @@ PATH.less = {
   out : PATH.dest + 'css/'
 };
 
-PATH.css = {
-    in  : PATH.source + 'css/**/*.css',
-    out : PATH.dest   + 'css/'
-};
-
 PATH.html = {
   in  : PATH.src  + '*.html',
   out : PATH.dest + './'
@@ -58,10 +53,9 @@ PATH.njk = {
   out : PATH.src
 };
 
-
 // OPTIONS ============================================
 //
-// SERVER
+
 const SYNC_CONFIG = {
   port   : 3333,
   browser: "chrome",
@@ -75,41 +69,15 @@ const SYNC_CONFIG = {
   notify : false
 };
 
-
-
-// LESS options
 var LESS_PREFIXER = new LessAutoPrefix({
         browsers: ['last 5 versions', 'ie 9', 'Firefox 14']
     });
 
-
-
-// NUNJUCKS options
 var NUNJUCKS_DEFAULTS = {
     path: 'src/_templates/'
-    // envOptions: {
-    //     watch: false
-    // }
 };
 
-
-// CSS ================================================
-//
-gulp.task('css', function() {
-    // console.log('*************************');
-    // console.log('*** Starting CSS task ***');
-    // console.log('*************************');
-
-    return gulp.src(PATH.css.in)
-        .pipe(gulp.dest(PATH.css.out))
-        ;
-});
-
 gulp.task('less', function() {
-    // console.log('**************************');
-    // console.log('*** Starting LESS task ***');
-    // console.log('**************************');
-
     return gulp.src(PATH.less.all)
         .pipe(changed(PATH.less.out))
         .pipe(plumber( function (err) {
@@ -119,75 +87,57 @@ gulp.task('less', function() {
             console.log(err);
             this.emit('end');
         }))
-        .pipe(sourcemaps.init())
+        .pipe(gulpif(argv.prod, sourcemaps.init()))
         .pipe(less({
             paths   : [PATH.less.in],
             plugins : [LESS_PREFIXER]
         }))
         .pipe(rename('style.css'))
         .pipe(gulpif(argv.prod, cleanCSS()))
-        .pipe(sourcemaps.write())
+        .pipe(gulpif(argv.prod, sourcemaps.write()))
         .pipe(gulp.dest(PATH.less.out))
         .pipe(browsersync.reload({ stream: true }))
         ;
 });
 
-gulp.task('styles', ['css', 'less']);
-
-
-// IMAGES ==============================================
-//
 
 gulp.task('images', function() {
-    // console.log('****************************');
-    // console.log('*** Starting IMAGES task ***');
-    // console.log('****************************');
 
     return gulp.src(PATH.images.in)
-        //.pipe(changed(PATH.images.out))
         .pipe(gulpif(argv.prod, imagemin()))
         .pipe(gulp.dest(PATH.images.out))
         ;
 });
 
-gulp.task('pictures', ['images']);
-
-// TEMPLATING ==========================================
-//
 
 gulp.task('nunjucks', function() {
-    // console.log('******************************');
-    // console.log('*** Starting NUNJUCKS task ***');
-    // console.log('******************************');
 
-    // var stream = gulp.src(PATH.tpl.in)
     return gulp.src(PATH.njk.in)
         .pipe(changed(PATH.njk.out))
         .pipe(nunjucksRender(NUNJUCKS_DEFAULTS))
         .pipe(gulp.dest(PATH.njk.out))
         ;
-    // return stream;
 });
 
-// handle html
 gulp.task('html', ['nunjucks'], function() {
-    // console.log('**************************');
-    // console.log('*** Starting HTML task ***');
-    // console.log('**************************');
-
     return gulp.src(PATH.html.in)
         .pipe(changed(PATH.html.out))
         .pipe(gulp.dest(PATH.html.out))
         ;
 });
 
-// OTHER ===============================================
-//
+gulp.task('watcher', function() {
+  gulp.watch(PATH.images.in, ['images']);
+  gulp.watch(PATH.less.in, ['less']);
+  gulp.watch([PATH.njk.njk, PATH.njk.in] ['html', browsersync.reload]);
+})
+
+
 // BUILD
 gulp.task('build',
 
-    [   'styles',
-        'pictures',
+    [   'less',
+        'images',
         'html'
     ],
 
@@ -212,30 +162,14 @@ gulp.task('browsersync', function() {
 });
 
 
+
+
+
 // default task
-gulp.task('default', ['browsersync'], function() {
 
-    // image changes
-    gulp.watch(
-        [
-            PATH.images.in
-        ],                     ['pictures']);
 
-    // css changes
-    gulp.watch(PATH.css.in,    ['css']);
+gulp.task('default', ['browsersync', 'build', 'watcher']);
 
-    // less changes
-    gulp.watch(PATH.less.in,   ['less']);
 
-    // nunjucks changes
-   // gulp.watch([PATH.njk.njk, PATH.njk.in],   ['nunjucks', browsersync.reload]);
 
-    // html changes
-    // gulp.watch(PATH.html.in,   ['html', browsersync.reload]);
-    gulp.watch(
-        [
-            PATH.njk.njk,
-            PATH.njk.in
-        ],                     ['html', browsersync.reload]);
 
-});
